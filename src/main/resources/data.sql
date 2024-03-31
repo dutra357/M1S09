1c898647a6d4830f06b2b3e8619e399e3f8bef3e
David Dutra <dadutra@hotmail.com>
2024-03-31 11:40:32 -0300

    Implementados os scripts de SQL, schema e data - M1S09

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO livro (titulo, autor, anoPublicacao) VALUES ('História de Roma', 'Tito Lívio', 1998);
+
+INSERT INTO membro (nome, endereco, telefone) VALUES ('Laura Alves', 'Rua José Vieira', '1234-5678');
+
+INSERT INTO bibliotecario (nome, email) VALUES ('José da Silva', 'jose@dasilva.com');
+
+INSERT INTO visitante (nome, telefone) VALUES ('David Alves', '9874-5632')
